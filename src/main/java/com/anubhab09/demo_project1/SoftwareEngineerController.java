cce9f2c079a76665d1aba05b90d3d346e9942c2e
Anubab Saha <110291595+Anubhab09@users.noreply.github.com>
2025-11-09 14:26:58 +0530

    Update SoftwareEngineerController.java

--- src/main/java/com/anubhab09/demo_project1/SoftwareEngineerController.java
+++ src/main/java/com/anubhab09/demo_project1/SoftwareEngineerController.java
@@ -1,5 +1,6 @@
 package com.anubhab09.demo_project1;
 
+// Simply just to test an api
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
